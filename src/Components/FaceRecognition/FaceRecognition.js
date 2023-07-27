110e40ab13ea2a2d20d898063181cbe33a05834e
Mohammad Qasim <qassy02@gmail.com>
2023-07-28 02:27:34 +0530

    updated packages

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -1,27 +1,45 @@
 import React from "react";
-import './FaceRecognition.css'
+import "./FaceRecognition.css";
+import image from "./image.jpg";
+const FaceRecognition = ({ imageUrl, box, width, height }) => {
+  console.log("faceRcognition", box);
+  return (
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img
+          id="inputimage"
+          alt=""
+          src={imageUrl}
+          // src={image}
+          // width="500px"
+          width="auto"
+          height="auto"
+        />
+        {/* <div
+          className="bounding-box"
+          style={{
+            top: box.topRow + "px",
+            right: box.rightCol + "px",
+            bottom: box.bottomRow + "px",
+            left: box.leftCol + "px",
+          }}
+        ></div> */}
 
-const FaceRecognition = ({imageUrl,box}) => {
-    return(
-        <div className="center ma">
-            <div className="absolute mt2">
-                <img 
-                    id='inputimage' 
-                    alt='' 
-                    src={imageUrl} 
-                    width='500px' 
-                    height='auto'
-                />
-                <div className="bounding-box" 
-                    style ={{
-                            top: box.topRow,
-                            right: box.rightCol,
-                            bottom: box.bottomRow,
-                            left: box.leftCol
-                            }}
-                ></div>  
-            </div>
-        </div>
-    )
-}
+        {/* for muliple images */}
+        {box.map((face, index) => (
+          <div
+            key={index}
+            className="bounding-box"
+            style={{
+              top: face.top,
+              right: face.right,
+              bottom: face.bottom,
+              left: face.left,
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default FaceRecognition;
